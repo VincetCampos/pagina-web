15c99813392d357afb90a6a11bcee1c594051a6f
VincetCampos <>
2020-04-02 21:35:11 -0600

    mi primera pagina web

---
+++ historiadelacomputacion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historia de la computación</title>
+        <style type="text/css">
+        body{
+            color:gold;
+            background-color: indigo
+        }
+        </style>
+</head>
+<body>
+    <p>
+       <h2 align = "center"> Historia de la Computación </h2></p>
+	
+    <p align= "justify">Uno de los primeros dispositivos mecánicos para contar fue el ábaco, cuya historia se remonta a las antiguas civilizaciones griega y romana. Este dispositivo es muy sencillo, consta de cuentas ensartadas en varillas que a su vez están montadas en un marco rectangular.
+    </p>
+    <p align= "justify">Otro de los inventos mecánicos fue la Pascalina inventada por Blaise Pascal (1623 - 1662) de Francia y la de Gottfried Wilhelm von Leibniz (1646 - 1716) de Alemania. Con estas máquinas, los datos se representaban mediante las posiciones de los engranajes, y los datos se introducían manualmente estableciendo dichas posiciones finales de las ruedas, de manera similar a como leemos los números en el cuentakilómetros de un automóvil.
+    </p>
+    <center>
+        <img src= "picture/imagen_1.jpg"width="600" height="300"/> 
+        <br>
+        <video src="video/H_computación.mp4" width="800" height="400" autoplay controls></video>
+    </center>
+</body>
+
+
+
+</html>
